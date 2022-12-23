c74791720eeb81bb3a1b3ea1a3a4a00373473054
razhzq <razhaziq@gmail.com>
2022-12-23 17:49:05 +0800

    nft compliance campaign

--- contracts/AlystCampaign.sol
+++ contracts/AlystCampaign.sol
@@ -1,6 +1,10 @@
 pragma solidity ^0.8.15;
 
 
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+
 // interface NOTEInterface {
 //     function balanceOf(address account) external view returns (uint256);
 //     function transfer(address to, uint256 amount) external returns (bool);
@@ -11,9 +15,13 @@ pragma solidity ^0.8.15;
 
 
 
-contract AlystCampaign {
+contract AlystCampaign is ERC721URIStorage {
+
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
 
     string public campaignName;
+    string private campaignNFTURI;
     uint public campaignTargetAmount;
     uint public campaignFundedAmount;
     uint public campaignPeriod;
@@ -35,7 +43,10 @@ contract AlystCampaign {
 
 
 
-    constructor(string memory _campaignName, uint _campaignTargetAmount, uint _campaignPeriod) {
+    constructor(string memory _campaignName, 
+                string memory _campaignSymbol, 
+                uint _campaignTargetAmount, 
+                uint _campaignPeriod) ERC721(_campaignName, _campaignSymbol) {
         campaignName = _campaignName;
         campaignTargetAmount = _campaignTargetAmount;
         campaignPeriod = _campaignPeriod;
@@ -47,8 +58,9 @@ contract AlystCampaign {
     function pledgeToCampaign(uint _amount) public payable {
         require(msg.value > 0, "amount cannot be zero");
         // NOTE.allowance(msg.sender, address(this));
-
         // NOTE.transferFrom(msg.sender, address(this), _amount);
+
+        _tokenIds.increment();
 
         if (!userHasPledged[msg.sender]) {
              pledgers.push(msg.sender);
@@ -57,12 +69,16 @@ contract AlystCampaign {
         userToPledgeAmount[msg.sender] = msg.value;
         campaignFundedAmount = campaignFundedAmount + msg.value;
 
+        uint256 newItemId = _tokenIds.current();
+        _mint(msg.sender, newItemId);
+        _setTokenURI(newItemId, campaignNFTURI);
+
 
     }
 
     function refund() public {
         require(block.timestamp > campaignTimeOpen + campaignPeriod);
-        require(campaignTargetAmount !== campaignFundedAmount);
+        require(campaignTargetAmount != campaignFundedAmount);
         
         // check amount invested 
         uint refundAmount = userToPledgeAmount[msg.sender];
@@ -86,6 +102,14 @@ contract AlystCampaign {
         // NOTE.transferFrom(address(this), alystTreasury, alystServiceCharge);
 
     }
+
+    function setCampaignNFTURI(string memory _nftURI) public {
+        require(campaignCreator == msg.sender);
+        campaignNFTURI = _nftURI;
+
+    }
+
+    
 
   
 
